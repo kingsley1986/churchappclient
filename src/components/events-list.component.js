2a9edd1fac2b4909668674a3dc32ef7f3f948c59
Kingsley <chukwumakingsley1@gmail.com>
2020-07-27 23:39:22 +0200

    working responsive on progress

--- src/components/events-list.component.js
+++ src/components/events-list.component.js
@@ -12,6 +12,17 @@ import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 
 const useStyles = (theme) => ({
+  contentContainer: {
+    // width: "100%",
+    // [theme.breakpoints.down("md")]: {
+    //   maxWidth: "85%",
+    // },
+    // [theme.breakpoints.down("sm")]: {
+    //   display: "grid",
+    //   gridTemplateColumns: "fr",
+    // },
+  },
+
   root: {
     display: "flex",
     flexWrap: "wrap",
@@ -20,8 +31,8 @@ const useStyles = (theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: 1000,
-    height: 950,
+    // width: 1000,
+    height: "auto",
   },
   icon: {
     color: "rgba(255, 255, 255, 0.54)",
@@ -49,10 +60,15 @@ class EventsList extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <GridList cellHeight={330} className={classes.gridList} spacing={8}>
-          <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
-            <ListSubheader component="div">December</ListSubheader>
-          </GridListTile>
+        <GridListTile key="Subheader" style={{ height: "auto" }}>
+          <ListSubheader component="div">December</ListSubheader>
+        </GridListTile>
+        <GridList
+          cellHeight={330}
+          cols={3}
+          className={classes.gridList}
+          spacing={8}
+        >
           {this.state.events.map((tile) => (
             <GridListTile key={tile.eventImage}>
               <img src={tile.eventImage} alt={tile.title} />
