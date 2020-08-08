4e85337cb2dd192c24dbed77a19c6d530c0deea9
Kingsley <chukwumakingsley1@gmail.com>
2020-08-08 18:37:07 +0200

    nice working eventlists

--- src/components/events-list.component.js
+++ src/components/events-list.component.js
@@ -6,21 +6,15 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import GridList from "@material-ui/core/GridList";
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { Link } from "react-router-dom";
-
 import moment from "moment";
 
 export default function EventsList() {
@@ -31,9 +25,9 @@ export default function EventsList() {
       maxWidth: "auto",
     },
     media: {
-      height: 0,
+      height: 350,
       paddingTop: "56.25%", // 16:9
-      height: "100%",
+      // height: "100%",
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
@@ -52,12 +46,6 @@ export default function EventsList() {
       backgroundColor: red[500],
     },
   }));
-
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   useEffect(() => {
     axios
@@ -83,23 +71,32 @@ export default function EventsList() {
         {tileData.map((name, key) => {
           return (
             <Card
+              style={{ paddingBottom: "550px" }}
               component={Link}
               to={"/events/" + name._id + "/eventcomments"}
               key={Math.floor(Math.random() * new Date().getTime())}
             >
+              <h3
+                style={{
+                  background: "	#800000",
+                  color: "white",
+                  textAlign: "center",
+                }}
+              >
+                {name.title}
+              </h3>
+
               <CardHeader
                 avatar={
                   <Avatar aria-label="recipe" className={classes.avatar}>
                     R
                   </Avatar>
                 }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
+                title={"Starts:" + " " + moment(name.staringDate).format("lll")}
+                subheader={
+                  "Ends:" + " " + moment(name.closingDate).format("lll")
                 }
-                title={name.title}
-                subheader="September 14, 2016"
+                style={{ background: "#DCDCDC" }}
               />
               <CardMedia
                 className={classes.media}
@@ -107,38 +104,15 @@ export default function EventsList() {
                 title="Paella dish"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {name.description}
+                <Typography
+                  style={{ color: "black" }}
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {name.description.substring(0, 100)}
                 </Typography>
               </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-                <IconButton
-                  className={clsx(classes.expand, {
-                    [classes.expandOpen]: expanded,
-                  })}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </IconButton>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography paragraph>Method:</Typography>
-                  <Typography paragraph>
-                    Heat 1/2 cup of the broth in a pot until simmering, add
-                    saffron and set aside for 10 minutes.
-                  </Typography>
-                  <Typography paragraph>{name.description}</Typography>
-                </CardContent>
-              </Collapse>
             </Card>
           );
         })}
